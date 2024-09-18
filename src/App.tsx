eab54454da83bf22dce37dee3636284b1810d398
jellysoo97 <jellysoo97@gmail.com>
2024-09-19 00:18:55 +0900

    test: 슬랙 봇 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import '@/styles/global.css'
 
 function App() {
-  return <p className="text-base font-bold text-primary-600">test</p>
+  return <p className="text-2xl font-bold text-primary-600">test</p>
 }
 
 export default App
