1dbc08a0c4b46751ac0671b4227be7b01019e682
“chae-dahee” <cdh010126r@gmail.com>
2024-09-24 01:16:06 +0900

    feat: 아이콘, 이미지 추가

---
+++ src/components/icons/NextIcon.tsx
@@ -0,0 +1,30 @@
+import { cn } from '@/utils/cn'
+
+const NextIcon = ({ className, ...props }: React.ComponentProps<'svg'>) => {
+  return (
+    <svg
+      width="54px"
+      height="54px"
+      viewBox="0 0 1024 1024"
+      className={cn('h-8 w-8', className)}
+      {...props}
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M364.8 106.666667L298.666667 172.8 637.866667 512 298.666667 851.2l66.133333 66.133333L768 512z"
+          fill="#ffffff"
+        ></path>
+      </g>
+    </svg>
+  )
+}
+
+export default NextIcon
