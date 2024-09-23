206cd232bbf056a7cf6181e536d6d4b5c2663704
jellysoo97 <jellysoo97@gmail.com>
2024-09-23 17:18:21 +0900

    feat: 공통 IconButton 추가

---
+++ src/components/common/IconButton.tsx
@@ -0,0 +1,28 @@
+import { cn } from '@/utils/cn'
+import React from 'react'
+
+type Props = {
+  direction: 'horizontal' | 'vertical'
+  icon: string | React.ReactNode
+  text: string
+} & React.ComponentProps<'button'>
+
+const IconButton = ({ direction, icon, text, className, ...props }: Props) => {
+  return (
+    <button
+      className={cn(
+        direction === 'horizontal'
+          ? 'flex items-center'
+          : 'flex flex-col items-center',
+        'text-neutral-700',
+        className
+      )}
+      {...props}
+    >
+      {typeof icon === 'string' ? <img src={icon} alt="icon" /> : <>{icon}</>}
+      <span className={cn(!className && 'text-sm')}>{text}</span>
+    </button>
+  )
+}
+
+export default IconButton
