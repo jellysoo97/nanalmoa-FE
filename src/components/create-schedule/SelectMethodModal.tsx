3c9129af2a3eff52219dd72b3588ca26bd1cd103
jellysoo97 <jellysoo97@gmail.com>
2024-09-24 16:46:20 +0900

    fix: 음성 등록 link 수정

--- src/components/create-schedule/SelectMethodModal.tsx
+++ src/components/create-schedule/SelectMethodModal.tsx
@@ -10,13 +10,13 @@ const SelectMethodModal = ({ onClose }: Props) => {
     <Modal title="일정 등록 방법 선택" hasHelp onClose={onClose}>
       <div className="flex flex-1 flex-col items-center gap-y-2 p-3">
         <div className="grid w-full grid-cols-2 gap-x-2">
-          <Link to={path.AudioCreate} onClick={onClose}>
+          <Link to={path.AudioAbout} onClick={onClose}>
             <div className="flex flex-col items-center justify-center gap-y-1 rounded-xl bg-primary-coral py-6">
               <span className="text-2xl">🎙️</span>
               <span>자동 음성 등록</span>
             </div>
           </Link>
-          <Link to={path.PhotoCreate} onClick={onClose}>
+          <Link to={path.PhotoAbout} onClick={onClose}>
             <div className="flex flex-col items-center justify-center gap-y-1 rounded-xl bg-primary-yellow py-6">
               <span className="text-2xl">📷</span>
               <span>자동 사진 등록</span>
