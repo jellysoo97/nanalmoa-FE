5ce1877a61f55bbe5acc598649b398fad2c5b69b
jellysoo97 <jellysoo97@gmail.com>
2024-09-24 15:14:41 +0900

    style: 아이콘 수정

---
+++ src/components/icons/CloseIcon.tsx
@@ -0,0 +1,40 @@
+import { cn } from '@/utils/cn'
+
+const CloseIcon = ({ className, ...props }: React.ComponentProps<'svg'>) => {
+  return (
+    <svg
+      width="64px"
+      height="64px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={cn('h-8 w-8', className)}
+      {...props}
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {' '}
+        <rect width="24" height="24" fill="white"></rect>{' '}
+        <path
+          d="M7 17L16.8995 7.10051"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>{' '}
+        <path
+          d="M7 7.00001L16.8995 16.8995"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>{' '}
+      </g>
+    </svg>
+  )
+}
+
+export default CloseIcon
--- src/components/icons/SettingIcon.tsx
+++ src/components/icons/SettingIcon.tsx
@@ -9,15 +9,15 @@ const SettingIcon = ({ className, ...props }: React.ComponentProps<'svg'>) => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       stroke="currentColor"
-      stroke-width="0.36"
+      strokeWidth="0.1"
       className={cn('h-8 w-8', className)}
       {...props}
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         {' '}
