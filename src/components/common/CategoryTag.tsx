ae28cd8fa0921b0b39fbf7335beb2984fa3cc24f
paengjiwoo <shishalu04@gmail.com>
2024-09-25 23:39:34 +0900

    feat: categoryTag -> span 및 button 태그 모두 활용되도록 수정
    ! 'as' prop에 'button'을 보내 button 태그로 활용할 수 있습니다
    
    - React.HTMLAttributes<HTMLElement> 타입 & ...props 추가
      - HTML 속성들 prop 가능

--- src/components/common/CategoryTag.tsx
+++ src/components/common/CategoryTag.tsx
@@ -1,22 +1,29 @@
 import { Categories, CategoryEnum } from '@/types/category'
 import { cn } from '@/utils/cn'
 
-// backend에서 들어오는 정보 형태에 따라 수정
-
 type Props = {
   label: Categories
-}
+  className?: string
+  as?: 'span' | 'button'
+} & React.HTMLAttributes<HTMLElement>
 
-const CategoryTag = ({ label }: Props) => {
+const CategoryTag = ({
+  label,
+  className,
+  as: Component = 'span',
+  ...props
+}: Props) => {
   return (
-    <span
+    <Component
       className={cn(
         CategoryEnum[label],
-        'mr-1 rounded-lg px-2 py-1 text-xs text-white'
+        'mr-1 rounded-lg px-2 py-1 text-xs text-white',
+        className
       )}
+      {...props}
     >
       {label}
-    </span>
+    </Component>
   )
 }
 
