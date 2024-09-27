5ce143d12c47beb684e7e06fe7b4fbbfcff1ea6b
jellysoo97 <jellysoo97@gmail.com>
2024-09-27 14:40:59 +0900

    feat: 분석 결과 Carousel 추가

--- src/pages/create-schedule/AudioCreatePage.tsx
+++ src/pages/create-schedule/AudioCreatePage.tsx
@@ -1,7 +1,6 @@
 import StartAudio from '@/components/create-schedule/audio/StartAudio'
 import FailAudio from '@/components/create-schedule/audio/FailAudio'
 import SuccessAudio from '@/components/create-schedule/audio/SuccessAudio'
-// import SuccessPostAudio from '@/components/create-schedule/audio/SuccessPostAudio'
 import { useState } from 'react'
 import MediaAnalysisLoadingModal from '@/components/create-schedule/MediaAnalysisLoadingModal'
 import { useModal } from '@/hooks/use-modal'
@@ -10,32 +9,44 @@ import { useNavigate } from 'react-router-dom'
 import { useMutation } from '@tanstack/react-query'
 import { postUploadAudioFile } from '@/api/schedules/post-upload-audio-file'
 import {
+  PostSchedulesReq,
+  PostSchedulesRes,
   PostUploadAudioFileReq,
   PostUploadAudioFileRes,
 } from '@/types/schedules'
 import { AxiosError } from 'axios'
 import { QUERY_KEYS } from '@/constants/api'
+import { postSchedules } from '@/api/schedules/post-schedules'
+import SuccessPostAudio from '@/components/create-schedule/audio/SuccessPostAudio'
 
 const AudioCreate = () => {
   const navigate = useNavigate()
   const { closeModal } = useModal()
 
-  const [result, setResult] = useState<PostUploadAudioFileRes[]>()
+  const [results, setResults] = useState<PostUploadAudioFileRes>()
 
   const mutation = useMutation<
-    PostUploadAudioFileRes[],
+    PostUploadAudioFileRes,
     AxiosError,
     PostUploadAudioFileReq
   >({
     mutationKey: [QUERY_KEYS.POST_AUDIO_FILE],
     mutationFn: postUploadAudioFile,
     onSuccess: (res) => {
-      setResult(res)
+      setResults(res)
     },
     onError: (err) => {
       console.error('err', err)
-      setResult(undefined)
+      setResults(undefined)
     },
+  })
+  const {
+    mutate: createSchedules,
+    isSuccess: isCreateSchedulesSuccess,
+    isError: isCreateSchedulesError,
+  } = useMutation<PostSchedulesRes, AxiosError, PostSchedulesReq>({
+    mutationKey: [QUERY_KEYS.POST_SCHEDULES],
+    mutationFn: postSchedules,
   })
 
   const uploadAudio = (file: File) => {
@@ -47,10 +58,10 @@ const AudioCreate = () => {
 
   return (
     <div className="flex h-full flex-col p-5">
-      {!result && <StartAudio handlePost={uploadAudio} />}
+      {!results && <StartAudio handlePost={uploadAudio} />}
       {mutation.isPending && (
         <MediaAnalysisLoadingModal
-          text={`음성 분석 중입니다. \n잠시만 기다려주세요.`}
+          text={`음성 분석 중입니다. \n약 20~30초 정도 소요됩니다.\n 잠시만 기다려주세요.`}
           onClose={() => {
             closeModal()
             navigate(path.createSchedule.audio.about)
@@ -64,11 +75,15 @@ const AudioCreate = () => {
           }}
         />
       )}
-      {mutation.isSuccess && <SuccessAudio />}
-      {mutation.isError && <FailAudio />}
+      {mutation.isSuccess && results && !isCreateSchedulesSuccess && (
+        <SuccessAudio results={results} createSchedules={createSchedules} />
+      )}
+      {mutation.isError && !isCreateSchedulesSuccess && <FailAudio />}
 
       {/* post 등록완료 */}
-      {/* <SuccessPostAudio /> */}
+      {(isCreateSchedulesSuccess || isCreateSchedulesError) && (
+        <SuccessPostAudio />
+      )}
     </div>
   )
 }
