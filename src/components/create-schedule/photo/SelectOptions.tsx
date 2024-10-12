0c7fc25b53199a991da3306d4cdae4154d8f89ea
jellysoo97 <jellysoo97@gmail.com>
2024-10-12 20:07:02 +0900

    feat: 사진 등록 페이지 ui

---
+++ src/components/create-schedule/photo/SelectOptions.tsx
@@ -0,0 +1,61 @@
+import camera from '@/assets/imgs/camera.png'
+import { Button } from '@/components/common'
+
+type Props = {
+  title: string
+  description?: React.ReactNode
+  leftButtonText: string
+  rightButtonText: string
+  hasImage?: boolean
+  leftButtonCallback: () => void
+  rightButtonCallback: () => void
+}
+
+const SelectOptions = ({
+  title,
+  description,
+  leftButtonText,
+  rightButtonText,
+  hasImage,
+  leftButtonCallback,
+  rightButtonCallback,
+}: Props) => {
+  return (
+    <>
+      {hasImage && (
+        <div className="relative">
+          <div className="absolute -left-2 h-32 w-32 rounded-full bg-violet-300 blur-xl" />
+          <div className="bg-white-400 absolute left-4 top-4 h-20 w-20 rounded-full blur-lg" />
+          <img
+            src={camera}
+            alt="camera"
+            width={128}
+            height={128}
+            className="relative"
+          />
+        </div>
+      )}
+
+      <div className="flex flex-col gap-y-4">
+        <p className="mb-2 text-center text-lg font-bold">{title}</p>
+        {description && description}
+        <div className="flex flex-col gap-y-4">
+          <Button
+            text={leftButtonText}
+            theme="solid"
+            className="py-3"
+            onClick={leftButtonCallback}
+          />
+          <Button
+            text={rightButtonText}
+            theme="outline"
+            className="py-3"
+            onClick={rightButtonCallback}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SelectOptions
--- src/components/create-schedule/photo/UploadPhotoStep.tsx
+++ src/components/create-schedule/photo/UploadPhotoStep.tsx
@@ -1,12 +1,110 @@
-const UploadPhotoStep = () => {
+import { useModal } from '@/hooks/use-modal'
+import { usePhoto } from '@/hooks/use-photo'
+import { PostAnalyzeImageReq, PostAnalyzeImageRes } from '@/types/schedules'
+import { UseMutationResult } from '@tanstack/react-query'
+import { ChangeEvent, useRef } from 'react'
+import MediaAnalysisLoadingModal from '../MediaAnalysisLoadingModal'
+
+import { GalleryIcon } from '@/components/icons'
+import { SelectOptions } from '.'
+
+type Props = {
+  analyzeImageMutation: UseMutationResult<
+    PostAnalyzeImageRes,
+    Error,
+    PostAnalyzeImageReq,
+    unknown
+  >
+}
+
+const UploadPhotoStep = ({ analyzeImageMutation }: Props) => {
+  const videoRef = useRef<HTMLVideoElement | null>(null)
+  const photoRef = useRef<HTMLCanvasElement | null>(null)
+  const inputRef = useRef<HTMLInputElement | null>(null)
+  const { isModalOpen: isLoadingModalOpen, closeModal: closeLoadingModal } =
+    useModal()
+  const { photo, setPhoto, takePhoto } = usePhoto({ videoRef, photoRef })
+
+  const handleFileInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+
+    if (file) {
+      const url = URL.createObjectURL(file)
+      setPhoto({ url, file })
+    }
+  }
+  const handleAnalyze = () => {
+    if (photo) {
+      analyzeImageMutation.mutate({
+        image: photo.file,
+        currentDateTime: new Date(),
+      })
+    }
+  }
+
   return (
     <>
-      <div>step 2</div>
+      {!photo ? (
+        <div className="relative">
+          <video ref={videoRef} autoPlay className="max-h-80 w-full" />
+          <div className="flex items-center justify-between bg-neutral-200 px-3 py-4">
+            <button
+              className="flex h-10 w-10 items-center justify-center rounded-full bg-neutral-100"
+              onClick={() => inputRef.current?.click()}
+            >
+              <GalleryIcon className="h-5 w-5" />
+            </button>
+            <button className="relative flex h-10 w-10" onClick={takePhoto}>
+              <div className="absolute inline-flex h-full w-full animate-ping rounded-full bg-neutral-100 opacity-75" />
+              <div className="relative inline-flex h-10 w-10 rounded-full bg-white">
+                <div className="absolute left-1 top-1 h-8 w-8 rounded-full border-2 border-neutral-400 bg-transparent" />
+              </div>
+            </button>
+            <div className="h-10 w-10" />
+          </div>
 
-      {/* <MediaAnalysisLoadingModal
-        text={`사진 분석 중입니다. \n잠시만 기다려주세요.`}
-        onClose={() => {}}
-      /> */}
+          <input
+            ref={inputRef}
+            type="file"
+            accept="image/*"
+            hidden
+            onChange={handleFileInput}
+            onClick={(e) => {
+              const input = e.target as HTMLInputElement
+              input.value = ''
+            }}
+          />
+          <canvas ref={photoRef} hidden />
+        </div>
+      ) : (
+        <div className="flex w-full flex-col gap-y-5">
+          <img src={photo.url} alt="photo" width={400} height={300} />
+
+          <SelectOptions
+            title="원하시는 사진이 맞나요?"
+            description={
+              <p className="text-center text-sm text-neutral-600">
+                선택하신 사진이 일정 분석에 사용됩니다
+              </p>
+            }
+            leftButtonText="네, 맞습니다"
+            rightButtonText="아니요, 재촬영하겠습니다"
+            leftButtonCallback={handleAnalyze}
+            rightButtonCallback={() => {
+              setPhoto(null)
+            }}
+          />
+        </div>
+      )}
+
+      {isLoadingModalOpen && (
+        <MediaAnalysisLoadingModal
+          text={`사진 분석 중입니다. \n잠시만 기다려주세요.`}
+          onLeftButtonClick={() => {}}
+          onRightButtonClick={() => {}}
+          onClose={closeLoadingModal}
+        />
+      )}
     </>
   )
 }
