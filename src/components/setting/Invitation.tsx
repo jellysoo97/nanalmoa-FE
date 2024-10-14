ae035021c9fce5016ccdc098dd9444751a61a335
팽지우 <paengzw@gmail.com>
2024-10-14 23:22:36 +0900

    chore: 주석 위치 수정

--- src/components/setting/Invitation.tsx
+++ src/components/setting/Invitation.tsx
@@ -15,8 +15,8 @@ const inviteClasses: Record<inviteTypes, string> = {
   PENDING: 'text-blue-500 bg-blue-200',
 }
 
-/* manager 타입이면 true, group 타입이면 false를 반환합니다 */
 const Invitation = ({ item }: Props) => {
+  /* manager 타입이면 true, group 타입이면 false를 반환합니다 */
   const isManagerInvitation = (
     item: IManagerInvitation | getGroupInvitationRes
   ): item is IManagerInvitation => {
