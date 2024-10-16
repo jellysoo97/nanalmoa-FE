70a6ead7bcccb33a6d8ac3d29ef72c7298de2332
jellysoo97 <jellysoo97@gmail.com>
2024-10-16 23:53:15 +0900

    chore: 알림 더미데이터 삭제

--- src/components/common/notification/Notification.tsx
+++ src/components/common/notification/Notification.tsx
@@ -1,12 +1,6 @@
 import { getInvitationsUser } from '@/api/invitations/get-invitations-user'
 import { QUERY_KEYS } from '@/constants/api'
-import {
-  GetInvitationsUserRes,
-  IInvitation,
-  InvitationRoleEnum,
-  InvitationStatusEnum,
-  InvitationTypeEnum,
-} from '@/types/invitations'
+import { GetInvitationsUserRes } from '@/types/invitations'
 import { cn } from '@/utils/cn'
 import { getAccessToken } from '@/utils/handle-token'
 import { useQuery } from '@tanstack/react-query'
@@ -14,123 +8,8 @@ import { AxiosError } from 'axios'
 import { useState } from 'react'
 import { IconButton } from '..'
 import { NotificationIcon } from '../../icons'
-import NotificationItem from './NotificationItem'
 import Toast from '../Toast'
-
-const DUMMY: Array<IInvitation> = [
-  {
-    id: 1,
-    type: InvitationTypeEnum.Group,
-    role: InvitationRoleEnum.GROUP_MEMBER,
-    status: InvitationStatusEnum.PENDING,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    inviterUuid: '1',
-    inviterName: '초대 발신자1',
-    inviteeUuid: '2',
-    inviteeName: '초대 수신자1',
-    groupId: '1',
-    groupName: '그룹명1',
-  },
-  {
-    id: 2,
-    type: InvitationTypeEnum.Manager,
-    role: InvitationRoleEnum.SUBORDINATE,
-    status: InvitationStatusEnum.PENDING,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    inviterUuid: '1',
-    inviterName: '초대 발신자2',
-    inviteeUuid: '2',
-    inviteeName: '초대 수신자2',
-    groupId: '',
-    groupName: '',
-  },
-  {
-    id: 3,
-    type: InvitationTypeEnum.Manager,
-    role: InvitationRoleEnum.MANAGER,
-    status: InvitationStatusEnum.ACCEPTED,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    inviterUuid: '1',
-    inviterName: '초대 발신자1',
-    inviteeUuid: '2',
-    inviteeName: '초대 수신자1',
-    groupId: '1',
-    groupName: '그룹명1',
-  },
-  {
-    id: 4,
-    type: InvitationTypeEnum.Manager,
-    role: InvitationRoleEnum.MANAGER,
-    status: InvitationStatusEnum.REJECTED,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    inviterUuid: '1',
-    inviterName: '초대 발신자1',
-    inviteeUuid: '2',
-    inviteeName: '초대 수신자1',
-    groupId: '1',
-    groupName: '그룹명1',
-  },
-  {
-    id: 5,
-    type: InvitationTypeEnum.Group,
-    role: InvitationRoleEnum.GROUP_ADMIN,
-    status: InvitationStatusEnum.REJECTED,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    inviterUuid: '1',
-    inviterName: '초대 발신자1',
-    inviteeUuid: '2',
-    inviteeName: '초대 수신자1',
-    groupId: '1',
-    groupName: '그룹명1',
-  },
-  {
-    id: 6,
-    type: InvitationTypeEnum.Group,
-    role: InvitationRoleEnum.GROUP_ADMIN,
-    status: InvitationStatusEnum.ACCEPTED,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    inviterUuid: '1',
-    inviterName: '초대 발신자1',
-    inviteeUuid: '2',
-    inviteeName: '초대 수신자1',
-    groupId: '1',
-    groupName: '그룹명1',
-  },
-  {
-    id: 7,
-    type: InvitationTypeEnum.Group,
-    role: InvitationRoleEnum.GROUP_MEMBER,
-    status: InvitationStatusEnum.ACCEPTED,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    inviterUuid: '1',
-    inviterName: '초대 발신자1',
-    inviteeUuid: '2',
-    inviteeName: '초대 수신자1',
-    groupId: '1',
-    groupName: '그룹명1',
-  },
-  {
-    id: 8,
-    type: InvitationTypeEnum.Manager,
-    role: InvitationRoleEnum.SUBORDINATE,
-    status: InvitationStatusEnum.REJECTED,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    inviterUuid: '1',
-    inviterName: '초대 발신자1',
-    inviteeUuid: '2',
-    inviteeName: '초대 수신자1',
-    groupId: '1',
-    groupName: '그룹명1',
-  },
-]
+import NotificationItem from './NotificationItem'
 
 const Notification = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
@@ -160,11 +39,11 @@ const Notification = () => {
               'flex max-h-80 min-h-24 w-60 flex-col overflow-hidden overflow-y-auto rounded-md border border-neutral-300 bg-neutral-100'
             )}
           >
-            {DUMMY.length === 0 ? (
+            {invitations.length === 0 ? (
               <p className="mt-10 text-center text-sm">알림이 없습니다.</p>
             ) : (
               <>
-                {DUMMY?.map((invitation) => (
+                {invitations?.map((invitation) => (
                   <NotificationItem
                     key={invitation.id}
                     notification={invitation}
