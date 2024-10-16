69dbe324840535be488ba1dea8fba583e7914681
jellysoo97 <jellysoo97@gmail.com>
2024-10-16 23:44:02 +0900

    feat: 초대 수락, 거절 로직 추가

--- src/hooks/use-notification.ts
+++ src/hooks/use-notification.ts
@@ -3,13 +3,21 @@ import { patchGroupReject } from '@/api/group/patch-group-reject'
 import { patchManagerAccept } from '@/api/manager/patch-manager-accept'
 import { patchManagerReject } from '@/api/manager/patch-manager-reject'
 import { QUERY_KEYS } from '@/constants/api'
+import { errorMessages } from '@/constants/validation'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'react-toastify'
 
 export const useNotification = () => {
   const queryClient = useQueryClient()
 
   const managerAcceptMutation = useMutation({
     mutationFn: patchManagerAccept,
+    onSuccess: () => {
+      toast.success('초대가 수락되었습니다.')
+    },
+    onError: () => {
+      toast.error(errorMessages.default)
+    },
     onSettled: () => {
       queryClient.invalidateQueries({
         queryKey: [QUERY_KEYS.GET_INVITATIONS_USER],
@@ -18,6 +26,12 @@ export const useNotification = () => {
   })
   const managerRejectMutation = useMutation({
     mutationFn: patchManagerReject,
+    onSuccess: () => {
+      toast.success('초대가 거절되었습니다.')
+    },
+    onError: () => {
+      toast.error(errorMessages.default)
+    },
     onSettled: () => {
       queryClient.invalidateQueries({
         queryKey: [QUERY_KEYS.GET_INVITATIONS_USER],
@@ -26,6 +40,12 @@ export const useNotification = () => {
   })
   const groupAcceptMutation = useMutation({
     mutationFn: patchGroupAccept,
+    onSuccess: () => {
+      toast.success('초대가 수락되었습니다.')
+    },
+    onError: () => {
+      toast.error(errorMessages.default)
+    },
     onSettled: () => {
       queryClient.invalidateQueries({
         queryKey: [QUERY_KEYS.GET_INVITATIONS_USER],
@@ -34,6 +54,12 @@ export const useNotification = () => {
   })
   const groupRejectMutation = useMutation({
     mutationFn: patchGroupReject,
+    onSuccess: () => {
+      toast.success('초대가 거절되었습니다.')
+    },
+    onError: () => {
+      toast.error(errorMessages.default)
+    },
     onSettled: () => {
       queryClient.invalidateQueries({
         queryKey: [QUERY_KEYS.GET_INVITATIONS_USER],
