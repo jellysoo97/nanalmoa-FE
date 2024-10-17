f1390bae16ea478841747181347033cea9ff2725
paengjiwoo <shishalu04@gmail.com>
2024-10-17 18:19:12 +0900

    chore: HomePage 컴포넌트 관리자 버튼 제거 버전 UI로 변경

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,15 @@
-import SideBar from '@/components/common/SideBar'
 import CalendarView from '@/components/schedule/CalendarView'
 import DailyView from '@/components/schedule/DailyView'
 import Tabs from '@/components/schedule/Tabs'
-import { useSideBar } from '@/hooks/use-side-bar'
 import { TabEnum } from '@/types/common'
 import { useState } from 'react'
 
 const HomePage = () => {
   const [activeTab, setActiveTab] = useState<TabEnum>(TabEnum.Daily)
   const tabs = Object.values(TabEnum)
-  const { isSidebarOpen, handleSideBar } = useSideBar()
 
   return (
     <div className="flex flex-1 flex-col gap-y-5">
-      <button onClick={handleSideBar}>관리자</button>
       <Tabs<TabEnum>
         activeTab={activeTab}
         setActiveTab={setActiveTab}
@@ -23,7 +19,6 @@ const HomePage = () => {
       {activeTab === TabEnum.Daily && <DailyView />}
 
       {activeTab === TabEnum.Monthly && <CalendarView />}
-      <SideBar isSidebarOpen={isSidebarOpen} toggleSidebar={handleSideBar} />
     </div>
   )
 }
